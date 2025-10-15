69cedeb82eeba8d56cdb615f8c02080aa41451dc
Sushma Mangalapally <sushma.m812@gmail.com>
2025-10-15 13:47:04 -0700

    Add changes

--- cypress/e2e/completest.cy.js
+++ cypress/e2e/completest.cy.js
@@ -49,34 +49,6 @@ describe('complete test', () => {
 				.should('have.class', 'focused')
 	});
 
-	// cypress/support/commands.ts
-	Cypress.Commands.add('login', (user) => {
-		cy.session(
-			user.email,                        // unique cache key
-			() => {                            
-	
-				cy.visit('http://localhost:5173'); 
-
-				cy.get('input[name=email]').clear().type(user.email);
-				cy.get('input[name=password]').clear().type(user.password);
-				cy.findByRole('button', { name: /next|continue/i }).click();
-
-				cy.findByRole('heading', { name: /enter.*code/i }).should('be.visible');
-				cy.get('input[name="otp-0"]').type('1');
-				cy.get('input[name="otp-1"]').type('2');
-				cy.get('input[name="otp-2"]').type('3');
-				cy.get('input[name="otp-3"]').type('4');
-				cy.get('input[name="otp-4"]').type('5');
-				cy.get('input[name="otp-5"]').type('6');
-
-				cy.findByText(/welcome to the home page!/i).should('be.visible');
-				cy.get('p.user-email').contains(user.email);
-			},
-			{
-			}
-		);
-	});
-
 	it('admin can and edit a row and saves changes', () => {
 		cy.login({
 			name: 'Adam the Admin',
--- cypress/e2e/smoke.cy.js
+++
@@ -1,6 +0,0 @@
-describe('homepage', () => {
-  it('loads and shows title', () => {
-    cy.visit('http://localhost:5173');   // your dev server URL
-    cy.contains('Log In').should('be.visible');
-  });
-});
