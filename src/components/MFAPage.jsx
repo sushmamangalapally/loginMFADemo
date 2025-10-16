35c47c88124fe7708c25fe4bd59bec11d6e481d9
Sushma Mangalapally <sushma.m812@gmail.com>
2025-10-15 21:28:36 -0700

    :lipstick: Fix UI bug issues

--- src/components/MFAPage.jsx
+++ src/components/MFAPage.jsx
@@ -5,12 +5,15 @@ import AuthOtpCodeInput from './AuthOtpCodeInput.jsx';
 export default function MFAPage() {
 	const { email } = useAuth();
 
+	const emailHidden = email ? email.replace(/(.{2})(.*)(?=@)/,
+		(match, p1, p2) => p1 + '*'.repeat(p2.length)) : '';
+
 	return (
 		<section className="login-page center-section">
 			<div className="card-container">
 				<div className="card">
 					<h1 className="title">Check your email</h1>
-					<h2>Please enter the six digit verification code we sent to {email}</h2>
+					<h2>Please enter the six digit verification code we sent to {emailHidden}</h2>
 					<AuthOtpCodeInput/>
 				</div>
 			</div>
